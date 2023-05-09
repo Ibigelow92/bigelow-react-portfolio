2d3ba680a81fa26be44f0495d621b4570c8f6ae6
Ibigelow92 <iobbigelow@gmail.com>
2023-05-09 16:02:31 -0700

    finished home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import "../styles/Navbar.css";
-import ReorderIcon from '@material-ui/icons'
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
-    return <div classname="navbar">
+    const [expandNavbar, setExpandNavbar] = useState(false);
+
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavbar(false);
+    }, [location]);
+
+    return <div classname="navbar" id={expandNavbar ? "open" : "closed"}>
         <div className="toggleButton"> 
-            <button></button>
+            <button onClick={() => {setExpandNavbar(prev => !prev);
+            }}>
+                <ReorderIcon />
+            </button>
         </div>
         <div className="links">
             <Link to="/"> Home </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import "../styles.Home.css";
 
 function Home() {
-    return <div>Home</div>;
+    return <div className="home">
+        <div className="about">
+            <h2> Hi, my name is Ira</h2>
+            <div className="prompt">
+                <p>and I am a beginning developer</p>
+                <FacebookIcon />
+                <LinkedInIcon />
+                <EmailIcon />
+                <GithubIcon />
+            </div>
+        </div>
+        <div className="skills">
+
+            <h2>Skills</h2>
+            <ol className="list">
+                <li className="item">
+                    <h3>Front-end</h3>
+                    <span>HTML, CSS, ReactJS, BootStrap</span>
+                </li>
+                <li className="item">
+                    <h3>Back-end</h3>
+                    <span>NodeJS, ExpressJS, MySQL, MongoDB, GraphQL, ApolloServer</span>
+                </li>
+            </ol>
+        </div>
+    </div>;
 }
 
 export default Home
