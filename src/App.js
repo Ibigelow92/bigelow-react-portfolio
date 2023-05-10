aeb7e326a8670a7c4dbccbd71b565864a99d1e3c
Ibigelow92 <iobbigelow@gmail.com>
2023-05-09 17:11:05 -0700

    did some work on experience page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// 45:51
+
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 function Experience() {
-    return <div>Home</div>;
+    return (
+        <div className="experience">
+            <VerticalTimeLine lineColoe="#3e497a">
+                <VerticalTimeLineElement 
+                    className="vertical-timeline-element--education" 
+                    date=""
+                    iconStyle={{background: "#3e497a", color: "#fff"}}>
+
+                    </VerticalTimeLineElement>
+            </VerticalTimeLine>
+        </div>
+    );
 }
 
 export default Experience
