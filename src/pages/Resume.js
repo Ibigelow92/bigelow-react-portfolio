7312760175650affd7ebcc90df14c38aeeb48f3f
Ibigelow92 <iobbigelow@gmail.com>
2023-05-11 16:34:29 -0700

    adding resume page

---
+++ src/pages/Resume.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Resume.css";
+
+
+function Resume() {
+    return <div className="resume">
+      <img src="../assets/tech-resume.jpg"></img>
+    </div>;
+}
+
+export default Footer;
